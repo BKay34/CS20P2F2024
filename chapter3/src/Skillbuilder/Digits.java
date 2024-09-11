e57af42b1c1acbff76de98a2cb38bc0ec454126f
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-09-11 11:55:06 -0600

    11:53 am 9/11/2024

---
+++ chapter3/src/Skillbuilder/Digits.java
@@ -0,0 +1,11 @@
+package Skillbuilder;
+
+import java.util.Scanner;
+
+public class Digits {
+
+public static void main(String[] args)
+{
+	Scanner UserInput = new Scanner(System.in); 
+	System.out.print("Please input a digit:		");
+	int n  =  UserInput.nextInt();
