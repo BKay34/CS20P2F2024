4ba617746fd83acd409c2f7ed07d18892a765c9d
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-09-12 10:24:18 -0600

    10:24

--- chapter3/src/Skillbuilder/Digits.java
+++ chapter3/src/Skillbuilder/Digits.java
@@ -3,9 +3,12 @@ package Skillbuilder;
 import java.util.Scanner;
 
 public class Digits {
-
 public static void main(String[] args)
+}
+
 {
 	Scanner UserInput = new Scanner(System.in); 
 	System.out.print("Please input a digit:		");
-	int n  =  UserInput.nextInt();
+	int n  =  UserInput.nextInt()
+	System.out.print("please input a second digit");
+	int n2 = UserInput.nextInt()
