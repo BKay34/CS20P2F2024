96809a9e1f22f3a926b881b4246dd4a2f4198b96
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-09-16 11:21:46 -0600

    11:21

--- chapter3/src/Skillbuilder/Digits.java
+++ chapter3/src/Skillbuilder/Digits.java
@@ -2,13 +2,16 @@ package Skillbuilder;
 
 import java.util.Scanner;
 
-public class Digits {
-public static void main(String[] args)
-}
-
+public class Digits 
 {
+	public static void main(String[] args)
+	{
+
 	Scanner UserInput = new Scanner(System.in); 
-	System.out.print("Please input a digit:		");
-	int n  =  UserInput.nextInt()
-	System.out.print("please input a second digit");
-	int n2 = UserInput.nextInt()
+	System.out.print("Please input a two digit number:		");
+	int n =  UserInput.nextInt();
+	int baseTen = (n / 10);
+	int ones = n%10;
+	System.out.println("Your baseTen is:" + baseTen + "your ones is:" + ones);
+	}
+}
---
+++ chapter3/src/Skillbuilder/gradeAverage.java
@@ -0,0 +1,9 @@
+package Skillbuilder;
+
+public class gradeAverage {
+}
+
+	public static void main(String[] args)
+	{
+		User.out.print("Please Input a grade:	")
+	}
