1d77a3212321c41960dc169df01f5c15525884c4
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-09-24 10:23:51 -0600

    10:23

---
+++ chapter3/src/mastery/projectmastery.java
@@ -0,0 +1,20 @@
+package mastery;
+
+import java.util.Scanner;
+
+public class projectmastery {
+/*
+	
+	Program: projectmastery.java          Last Date of this Revision: September 24/2024
+
+	Purpose: An application that can determine how much time you've used for designing, coding, debugging, and testing.
+
+	Author: Justin Fung,
+	School: CHHS
+	Course: Computer Programming 20
+	*/ 
+public static void main(String[] args)
+{
+	Scanner UserInput = new Scanner(System.in); 
+	System.out.print("Input a ");
+}
