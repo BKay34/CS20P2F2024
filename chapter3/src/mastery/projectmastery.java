094cfce2f400c91dbee39f0256d1dac43c9a6796
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-09-25 11:23:58 -0600

    1123

--- chapter3/src/mastery/projectmastery.java
+++ chapter3/src/mastery/projectmastery.java
@@ -1,5 +1,6 @@
 package mastery;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class projectmastery {
@@ -16,5 +17,31 @@ public class projectmastery {
 public static void main(String[] args)
 {
 	Scanner UserInput = new Scanner(System.in); 
-	System.out.print("Input a ");
+	NumberFormat nf = NumberFormat.getPercentInstance();
+
+	// declaration 
+	double t, d, c, debug;
+	System.out.print("Please input time spent on designing");
+	d = UserInput.nextDouble(); 
+	System.out.print("Please input a time spent on coding "); 
+	c = UserInput.nextDouble();
+	System.out.print("Please Input time spent debugging ");
+	debug = UserInput.nextDouble(); 
+	System.out.print("Please Input time spent on testing ");
+	t = UserInput.nextDouble();
+	
+	double total_t = t + d + c + debug; 
+	
+	d = d / total_t;
+    c = c / total_t;
+    t = t / total_t;
+	debug = debug / total_t; 
+	// printing out the data that was calculated
+	System.out.print("\ntask:      % time:");
+	System.out.print("\ndesigning     "+ nf.format(d));
+	System.out.print("\ncoding     "+ nf.format(c));		
+	System.out.print("\ndebugging     "+ nf.format(debug));
+	System.out.print("\ntesting     "+ nf.format(t));
+		
+}
 }
