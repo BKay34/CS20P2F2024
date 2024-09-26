a0bdd3cbff39314b77f5eb575a88efa388775ebf
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-09-26 10:23:51 -0600

    1023

---
+++ chapter3/src/mastery/Electionmastery.java
@@ -0,0 +1,11 @@
+package mastery;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Electionmastery {
+
+public static void main(String[] args)
+{
+Scanner UserInput = new Scanner(System.in);
+NumberFormat nf = NumberFormat.getPercentInstance(); 
