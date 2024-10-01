cc3aa18168d96591f402f3e93855ddff02acffee
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-10-01 10:18:50 -0600

    1018

--- chapter3/src/mastery/Electionmastery.java
+++ chapter3/src/mastery/Electionmastery.java
@@ -1,5 +1,6 @@
 package mastery;
 
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -8,4 +9,37 @@ public class Electionmastery {
 public static void main(String[] args)
 {
 Scanner UserInput = new Scanner(System.in);
-NumberFormat nf = NumberFormat.getPercentInstance(); 
+int doranyc, diegonyc, doranj, diegonj, doracc, diegocc;
+DecimalFormat deca = new DecimalFormat ("#.##");
+// Asking for votes. 
+System.out.println("Enter votes for Dora in New York: ");
+doranyc = UserInput.nextInt();
+System.out.println("Enter votes for Diego in New York: ");
+diegonyc = UserInput.nextInt(); 
+
+System.out.println("Enter votes for dora in New Jersey: ");
+doranj = UserInput.nextInt();
+System.out.println("Enter votes for Diego in New Jersey: "); 
+diegonj = UserInput.nextInt(); 
+
+System.out.println("Enter votes for Dora in Connecticut: ");
+doracc = UserInput.nextInt();
+System.out.println("Enter votes for Diego in Connecticut: ");
+diegocc = UserInput.nextInt();
+
+// calculating total votes
+int totvote1 = doranyc + doranj + doracc; 
+int totvote2 = diegonyc + diegonj + diegocc; 
+
+int totalvotes = totvote1 + totvote2; 
+
+double percentdora = ((double)totvote1 / (double)totalvotes);
+double percentdiego = ((double)totvote2 / (double)totalvotes);
+percentdora *= 100; 
+percentdiego *= 100;
+System.out.println("Candidate      vote      percentage");
+System.out.println("Dora           " + totvote1 +"        " + deca.format(percentdora)+"%");
+System.out.println("Diego          " + totvote2 +"        " + deca.format(percentdiego)+"%");
+System.out.println("totalvotes: " + totalvotes);
+}
+}
