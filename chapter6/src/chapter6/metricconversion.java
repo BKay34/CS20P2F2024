bf4b00d6d727115cb26c168debf444c9444e5e97
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-10-24 10:22:21 -0600

    1022

---
+++ chapter6/src/chapter6/metricconversion.java
@@ -0,0 +1,68 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class metricconversion {
+
+
+
+	public static void feetToCenti () {    
+		
+	}
+	
+	public static void inchToCenti () {
+		
+	}
+	
+	public static void yardToMeter () {
+		
+	}
+	
+	public static void mileToKilometer () {
+		
+	}
+	
+	public static void centiToInch () {
+		
+	}
+	
+	public static void centiToFeet () {
+		
+	}
+	
+	public static void meterToYards () {
+		
+	}
+	
+	public static void KilometerToMiles () {
+		
+	}
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+//declarations 
+	int usernumbr, conversion;
+	System.out.println("Please enter a number:	");
+	usernumbr = input.nextInt();
+	
+	//asking user to choose a conversion method 
+	System.out.println("Conversions: ");
+	System.out.println("Choose a conversion method 1-8:  ");
+	System.out.println("1. Inches to Centimeters ");
+	System.out.println("2. Feet to Centimeters ");
+	System.out.println("3. Yards to Meters ");
+	System.out.println("4. Miles to Kilometers ");
+	System.out.println("5. Centimeters to Inches ");
+	System.out.println("6. Centimeters to Feet ");
+	System.out.println("7. Meters to Yards ");
+	System.out.println("8. Kilometers to Miles ");
+	
+
+
+
+
+
+
+
+
+}
+}
