819a075106d2f072e692c4422ee3d1bed3eb0206
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-10-29 10:17:19 -0600

    1017

--- chapter6/src/chapter6/metricconversion.java
+++ chapter6/src/chapter6/metricconversion.java
@@ -6,36 +6,76 @@ public class metricconversion {
 
 
 
-	public static void feetToCenti () {    
+	public static void inchToCenti (int usernumbr) {    
+		//declaring variables for conversion calculations
+		Scanner input = new Scanner(System.in);
+		
+		
+		double conversion = usernumbr * 2.54;
 		
 	}
 	
-	public static void inchToCenti () {
-		
+	public static void feetToCenti () {
+		//declaring variables for conversion calculations
+				Scanner input = new Scanner(System.in);
+				double usernumbr = input.nextInt();
+				
+				double conversion = usernumbr * 30.54;
+				
 	}
 	
 	public static void yardToMeter () {
-		
+		//declaring variables for conversion calculations
+				Scanner input = new Scanner(System.in);
+				double usernumbr = input.nextInt();
+				
+				double conversion = usernumbr / 1.094;
+					
 	}
 	
 	public static void mileToKilometer () {
-		
+		//declaring variables for conversion calculations
+				Scanner input = new Scanner(System.in);
+				double usernumbr = input.nextInt();
+				
+				double conversion = usernumbr * 1.609;
+				
 	}
 	
 	public static void centiToInch () {
-		
+		//declaring variables for conversion calculations
+				Scanner input = new Scanner(System.in);
+				double usernumbr = input.nextInt();
+				
+				double conversion = usernumbr / 2.54;
+				
 	}
 	
 	public static void centiToFeet () {
-		
+		//declaring variables for conversion calculations
+				Scanner input = new Scanner(System.in);
+				double usernumbr = input.nextInt();
+				
+				double conversion = usernumbr / 30.48;
+				
 	}
 	
 	public static void meterToYards () {
-		
+		//declaring variables for conversion calculations
+				Scanner input = new Scanner(System.in);
+				double usernumbr = input.nextInt();
+				
+				double conversion = usernumbr * 1.094;
+				
 	}
 	
 	public static void KilometerToMiles () {
-		
+		//declaring variables for conversion calculations
+				Scanner input = new Scanner(System.in);
+				double usernumbr = input.nextInt();
+				
+				double conversion = usernumbr / 1.609;
+				
 	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -45,9 +85,9 @@ public class metricconversion {
 	usernumbr = input.nextInt();
 	
 	//asking user to choose a conversion method 
+	
 	System.out.println("Conversions: ");
 	System.out.println("Choose a conversion method 1-8:  ");
-	conversion = input.nextInt();
 	System.out.println("1. Inches to Centimeters ");
 	System.out.println("2. Feet to Centimeters ");
 	System.out.println("3. Yards to Meters ");
@@ -56,10 +96,12 @@ public class metricconversion {
 	System.out.println("6. Centimeters to Feet ");
 	System.out.println("7. Meters to Yards ");
 	System.out.println("8. Kilometers to Miles ");
-	
+	conversion = input.nextInt();
 	if(conversion == 1)
 	{
-		(inchToCenti)
+		inchToCenti();
+		System.out.println("Your new measurement is:  " + conversion + "cm");
+	
 	}
 
 
