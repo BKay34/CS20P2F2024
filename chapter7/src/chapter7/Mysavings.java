f8ffb7868cac5a731078c81fbf89ab090899de44
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-11-14 10:23:54 -0700

    1023

---
+++ chapter7/src/chapter7/Mysavings.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package chapter7;
+
+import java.util.Scanner;
+
+public class Mysavings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+//declarations
+int pennies, dimes, quarters, nickels, addcoin, removecoin, return;
+
+//asking user to select a method
+
+System.out.println("Make selection based on the bottom:  ");
+System.out.println("1. Show total in bank ");
+System.out.println("2. Add a penny ");
+System.out.println("3. Add a nickel ");
+System.out.println("4. Add a dime ");
+System.out.println("5. Add a quarter ");
+System.out.println("6. Take money out of bank ");
+System.out.println("Enter 0 to quit ");
+System.out.println("Enter you choice: 5 ");
+
+}
+}
