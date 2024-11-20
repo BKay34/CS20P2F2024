7d3e374e16fe79409b6f715fc75d29122723e6a5
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-11-20 11:56:03 -0700

    1156

--- chapter7/src/chapter7/skillbuilder1.java
+++ chapter7/src/chapter7/skillbuilder1.java
@@ -2,4 +2,14 @@ package chapter7;
 
 public class skillbuilder1 {
 
-}
+private static final double PI = 3.14;
+private double (radius);
+
+
+
+public static void main(String[] args) {
+	 circle spot = new circle();
+	 spot.setRadius(3);
+	 System.out.println("Circle radius: " + spot.getRadius());
+	 System.out.println("Circle circumference: " + spot.circumference());
+}}
