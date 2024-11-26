6b69533fa8f70e86c001b4a660fd4ae9e7de37f2
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-11-26 10:23:41 -0700

    1023

--- chapter7/src/chapter7/digitextractor.java
+++ chapter7/src/chapter7/digitextractor.java
@@ -3,6 +3,8 @@
  */
 package chapter7;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -13,7 +15,29 @@ public class digitextractor {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter a integer: ");
+		// declaring variables for place values
+		int num = input.nextInt();
+		
+		// setting up object for num
+		
+		Number placeV = new Number(num);
+		
+		System.out.println("Show (W)hole number. ");
+		System.out.println("Show (O)nes number. ");
+		System.out.println("Show (T)ens number. ");
+		System.out.println("Show (H)undreds number. ");
+		System.out.println("Quit. ");
+		
+		
+		//Start While loop to keep coming back to options until user inputs 0
+		char choice = ' ';
+		
+	
+	
+	
+	
 	}
-
 }
