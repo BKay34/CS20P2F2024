be3de5c10ba2899152e7db8022b032d5973472db
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-11-27 12:02:35 -0700

    1202

--- chapter7/src/chapter7/digitextractor.java
+++ chapter7/src/chapter7/digitextractor.java
@@ -25,17 +25,25 @@ public class digitextractor {
 		
 		Number placeV = new number(num);
 		
+		while(true) { 
 		System.out.println("Show (W)hole number. ");
 		System.out.println("Show (O)nes number. ");
 		System.out.println("Show (T)ens number. ");
 		System.out.println("Show (H)undreds number. ");
-		System.out.println("Quit. ");
+		System.out.println("(Q)uit. ");
 		
+		String choice = input.next();
+
+		if (choice.equals("Q")) {
+			break;
 		
 		//Start While loop to keep coming back to options until user inputs 0
 		char choice = ' ';
 		
-	
+	// when user chooses W O T H these following cases will occur
+		switch(choice) {
+		case 'W' : System.out.printLn("Your integer is: " + .wholeNum()); break;
+		}
 	
 	
 	
