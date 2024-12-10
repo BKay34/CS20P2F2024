c315c491dec587157bcfb79df55e8d0c1c37be37
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-12-10 10:18:20 -0700

    1018

--- chapter9/src/chapter9/oddsandeven.java
+++ chapter9/src/chapter9/oddsandeven.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class oddsandeven {
 
-
+	public static void main(String[] args) {
 	
 	//making random number
 	Random rand = new (Random)
@@ -14,10 +14,17 @@ public class oddsandeven {
 	
 	//creating a range for number 0-100
 	
+	final int min = 0;
+	final int max = 100;
+	int[] letterCounts = new int[max - min + 1];
 	
+	//generating the 25 integers randomly using range and assigning them in the array made to store them
+			for( int i = 0; i<25; i++) {
+				num[i] = (int)(Math.random() * RANGE);
+			}
 	
-	
-	
+	}
+}
 	
 	
 	
