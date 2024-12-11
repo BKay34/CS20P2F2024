741bcfd5276d5c662ac94fc332bb00d75cd7bcad
27315010 <27315010@L815WD220010.edu.cbe.ab.ca>
2024-12-11 11:20:51 -0700

    1120

--- chapter9/src/chapter9/oddsandeven.java
+++ chapter9/src/chapter9/oddsandeven.java
@@ -1,30 +1,54 @@
 package chapter9;
 
-import java.util.Random;
+import java.lang.Math;
 
 public class oddsandeven {
 
-	public static void main(String[] args) {
 	
-	//making random number
-	Random rand = new (Random)
-			
-	//array
+
+	public static void main(String[] args) {
+
+	
+		
+		//array
 	int[] num = new int[25];
 	
 	//creating a range for number 0-100
 	
 	final int min = 0;
 	final int max = 100;
-	int[] letterCounts = new int[max - min + 1];
+	final int range = max - min + 1;
 	
 	//generating the 25 integers randomly using range and assigning them in the array made to store them
 			for( int i = 0; i<25; i++) {
-				num[i] = (int)(Math.random() * RANGE);
+		
+				num[25] = (int)(Math.random() * range);
 			}
-	
+		// TO DISplay all odd numbers. 
+			System.out.println("ODD: ");
+			
+			for( int i = 0; i<25; i++);
+			if ((num[25] %2) != 0) {
+				System.out.println(num[25]);
+				System.out.println(" ");
+	}
+			//Blank output to create space between the even and odd numbers
+			System.out.println("");
+			//displaying all even numbers
+			System.out.println("EVEN:");
+			//checking from every number in the array to see which ones are even
+			for( int i = 0; i<25; i++) {
+				if ((num[i] % 2) == 0) {
+
+					System.out.print(num[i]);
+					System.out.print(" ");	
+			
+			
+			
 	}
 }
+	}
+	}
 	
 	
 	
@@ -43,6 +67,3 @@ public class oddsandeven {
 	
 	
 	
-	
-	
-}
